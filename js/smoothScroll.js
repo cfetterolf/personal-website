8674c4a9186f9866c8c9db925312d3e552aa0353
Chris <Chris@Chriss-MacBook-Pro-2.local>
2017-08-23 23:38:32 -0700

    update resume

---
+++ js/smoothScroll.js
@@ -0,0 +1,25 @@
+// Smooth Scroll on clicking nav items
+$('nav a').click(function () {
+  var $href = $(this).attr('href');
+  $('body').stop().animate({
+    scrollTop: $($href).offset().top - 30
+  }, 1000);
+  return false;
+});
+
+// Smooth scroll on logo click
+$('footer a').click(function () {
+  var $href = $(this).attr('href');
+  $('body').stop().animate({
+    scrollTop: $($href).offset().top - 30
+  }, 1000);
+  return false;
+});
+
+// Smooth scroll to top of clicked experience section
+$('.collapse').on('shown.bs.collapse', function(e) {
+  var $card = $(this).closest('.card');
+  $('html,body').animate({
+    scrollTop: $card.offset().top - 10
+  }, 500);
+});
