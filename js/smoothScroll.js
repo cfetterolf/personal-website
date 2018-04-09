9768d3724b8b9c8546d8159816ff2a84b1dafced
Chris <chris.fetterolf@gmail.com>
2018-04-09 15:52:22 -0400

    update resume

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,23 +1,22 @@
 
 // Smooth Scroll on clicking nav items
+/*
 $('nav a').click(function () {
   var $href = $(this).attr('href');
-  console.log($href);
-  $('html,body').stop().animate({
+  $('html,body').animate({
     scrollTop: $($href).offset().top - 30
   }, 1000);
-  return false;
 });
+
 
 // Smooth scroll on logo click
 $('footer a').click(function () {
   var $href = $(this).attr('href');
-  console.log($href);
   $('html,body').animate({
     scrollTop: $($href).offset().top - 30
   }, 1000);
-  return false;
 });
+*/
 
 // Smooth scroll to top of clicked experience section
 $('.collapse').on('shown.bs.collapse', function(e) {
@@ -26,3 +25,41 @@ $('.collapse').on('shown.bs.collapse', function(e) {
     scrollTop: $card.offset().top - 10
   }, 500);
 });
+
+$('footer a, nav a')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      //console.log(target);
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          //$target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            //$target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
