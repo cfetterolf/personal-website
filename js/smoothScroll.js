4dabf3b42851981e91eba6893a7f40073143534e
Chris Fetterolf <chris.fetterolf@gmail.com>
2018-05-25 13:07:18 -0400

    new project section is done

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -31,35 +31,25 @@ $('footer a, nav a')
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
-    // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
       location.hostname == this.hostname
     ) {
-      // Figure out element to scroll to
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      //console.log(target);
       if (target.length) {
-        // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 20
         }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
           var $target = $(target);
-          //$target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            //$target.focus(); // Set focus again
           };
         });
       }
     }
   });
-
